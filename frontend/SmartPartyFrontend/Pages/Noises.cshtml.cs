3150d6e0db0c64daa8b2a0beae4fae17bc8d7ba3
Winetq <winetq9679@gmail.com>
2022-12-06 19:45:43 +0100

    Add downloading data in csv and json for Noises

--- frontend/SmartPartyFrontend/Pages/Noises.cshtml.cs
+++ frontend/SmartPartyFrontend/Pages/Noises.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SmartPartyFrontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace SmartPartyFrontend.Pages;
 
@@ -21,6 +23,49 @@ public class NoisesModel : PageModel
         var response = _client.GetAsync("http://SI_175132_api/api/1/noiseSensor").Result;
         var body = response.Content.ReadFromJsonAsync<List<NoiseRecordModel>>().Result;
         if (body != null) Measurements = body;
+    }
+
+    public async Task<IActionResult> OnPostDownloadJson() 
+    {
+        var response = _client.GetAsync("http://SI_175132_api/api/1/noiseSensor").Result;
+        var noiseRecords = response.Content.ReadFromJsonAsync<List<NoiseRecordModel>>().Result;
+        if (!string.IsNullOrEmpty(Request.Form["startDateTime"]) && !string.IsNullOrEmpty(Request.Form["endDateTime"])) 
+        {
+            var startDateTime = Convert.ToDateTime(Request.Form["startDateTime"]);
+            var endDateTime = Convert.ToDateTime(Request.Form["endDateTime"]);
+            noiseRecords = noiseRecords.FindAll(record => record.MeasuredAt >= startDateTime && record.MeasuredAt <= endDateTime);
+        }
+        var jsonstr = System.Text.Json.JsonSerializer.Serialize(noiseRecords);
+        byte[] byteArray = System.Text.ASCIIEncoding.ASCII.GetBytes(jsonstr);
+        return File(byteArray, "application/force-download", "noiseRecords.json");
+    }
+
+    public async Task<IActionResult> OnPostDownloadCsv() 
+    {
+        var response = _client.GetAsync("http://SI_175132_api/api/1/noiseSensor").Result;
+        var noiseRecords = response.Content.ReadFromJsonAsync<List<NoiseRecordModel>>().Result;
+        if (!string.IsNullOrEmpty(Request.Form["startDateTime"]) && !string.IsNullOrEmpty(Request.Form["endDateTime"])) 
+        {
+            var startDateTime = Convert.ToDateTime(Request.Form["startDateTime"]);
+            var endDateTime = Convert.ToDateTime(Request.Form["endDateTime"]);
+            noiseRecords = noiseRecords.FindAll(record => record.MeasuredAt >= startDateTime && record.MeasuredAt <= endDateTime);
+        }
+
+        StringBuilder csv = new StringBuilder();
+
+        string[] columnNames = new string[] { "Id", "Value", "MeasuredAt", "SensorId" };
+        csv.AppendLine(string.Join(",", columnNames));
+
+        foreach (NoiseRecordModel record in noiseRecords) 
+        {
+            csv.AppendLine(string.Join(",", new string[] { 
+                record.Id,
+                record.Value.ToString(),
+                record.MeasuredAt.ToString(),
+                record.SensorId }));
+        }
+
+        return File(Encoding.ASCII.GetBytes(csv.ToString()), "application/force-download", "noiseRecords.csv");
     }
 
     public List<ChartDataset> GetValues()
